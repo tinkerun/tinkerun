7e7312a7ba91b3c329099f67bb7e6c5b250a5f35
billyct <billy_allen@126.com>
2021-03-12 16:43:19 +0800

    chore: update the electron-builder settings

--- builder.js
+++ builder.js
@@ -2,17 +2,36 @@ const path = require('path')
 
 const appPkg = path.join(__dirname, 'packages/app')
 const webPkg = path.join(__dirname, 'packages/web')
+const assetsDir = path.join(__dirname, 'assets')
 
 module.exports = {
+  appId: 'com.tinkerun.Tinkerun',
   productName: 'Tinkerun',
   copyright: 'Copyright © 2021 billyct',
-  compression: 'store',
+
+  extraMetadata: {
+    name: 'Tinkerun',
+    description: 'An application that lets you run code within your favorite PHP application - either locally or via SSH.',
+    homepage: 'https://github.com/tinkerun/tinkerun',
+    author: {
+      name: 'billyct',
+      email: 'billyct2012@gmail.com',
+    },
+    repository:{
+      url: 'https://github.com/tinkerun/tinkerun',
+    },
+  },
+
   directories: {
+    buildResources: assetsDir,
     app: appPkg,
     output: 'build',
   },
-  mac:{
-    electronLanguages: ['en', 'zh'],
+  mac: {
+    category: 'public.app-category.developer-tools',
+    icon: path.join(assetsDir, 'icons/mac/icon.icns'),
+    electronLanguages: ['en', 'zh', 'zh-CN'],
+    target: 'zip',
   },
   files: [
     '!**/*',
