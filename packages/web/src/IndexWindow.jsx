d7060bd13d9d0bb891a0c2441b60e4a50895f1aa
billyct <billy_allen@126.com>
2021-03-12 21:13:28 +0800

    fix: resize window will break the minSize of Split

--- packages/web/src/IndexWindow.jsx
+++ packages/web/src/IndexWindow.jsx
@@ -1,3 +1,4 @@
+import {useEffect, useRef} from 'react'
 import {Pane} from 'evergreen-ui'
 import Split from 'react-split'
 import {Route, Switch} from 'wouter'
@@ -7,21 +8,40 @@ import Inspire from './components/Inspire'
 import ConnectionPage from './components/ConnectionPage'
 import WindowContainer from './components/WindowContainer'
 
-const IndexWindow = () => (
-  <WindowContainer>
-    <Pane
-      is={Split}
-      display='flex'
-      sizes={[45, 55]}
-      minSize={[240, 300]}
-    >
-      <Sidebar/>
-      <Switch>
-        <Route path='/' component={Inspire}/>
-        <Route path='/connections/:id' component={ConnectionPage}/>
-      </Switch>
-    </Pane>
-  </WindowContainer>
-)
+const IndexWindow = () => {
+  const splitRef = useRef()
+
+  useEffect(() => {
+    const listener = (e) => {
+      const split = splitRef.current.split
+      // https://github.com/nathancahill/split/issues/87#issuecomment-311248421
+      split.setSizes(split.getSizes())
+    }
+
+    window.addEventListener('resize', listener)
+
+    return () => {
+      window.removeEventListener('resize', listener)
+    }
+  }, [])
+
+  return (
+    <WindowContainer>
+      <Pane
+        is={Split}
+        display='flex'
+        sizes={[45, 55]}
+        minSize={260}
+        ref={splitRef}
+      >
+        <Sidebar/>
+        <Switch>
+          <Route path='/' component={Inspire}/>
+          <Route path='/connections/:id' component={ConnectionPage}/>
+        </Switch>
+      </Pane>
+    </WindowContainer>
+  )
+}
 
 export default IndexWindow
